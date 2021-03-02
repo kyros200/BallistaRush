42678ab6a1a579faf589fb7af488a911362192b5
rafael najjar <rafaelnajjar@gmail.com>
2021-03-02 00:30:55 -0300

    Added Main Menu Scene, added scripts to the buttons

---
+++ Assets/Scripts/Session.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject itExists = GameObject.Find("Session");
+
+        if (FindObjectsOfType<Session>().Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
